2e234ea3d3fd5b9790ee80d1fbf4f83548d497da
jamesandyward <67004358+jamesandyward@users.noreply.github.com>
2020-06-19 16:36:00 +0100

    Update chromosomes.sh

--- chromosomes.sh
+++ chromosomes.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-### Argument is the name of the files generated from plink, if they were cattle.bed etc. then the argument would be "cattle", w/o the "".
+### First argument is the name of the files generated from plink, if they were clean_data.bed etc. then the argument would be "clean_data", w/o the "".
+### Second argument is the name you want the output file to be, for example "cattle", w/o the "".
 
 args=("$@")
 for chr in $(seq 1 29); do
@@ -8,5 +9,5 @@ for chr in $(seq 1 29); do
            --cow \
            --chr $chr \
 	   --make-bed \
-           --out ${args[0]}.chr$chr ;
+           --out ${args[1]}.chr$chr ;
 done
