3fa7c5785437b45629c21fa26809073688b4e9fd
jamesandyward <67004358+jamesandyward@users.noreply.github.com>
2020-06-19 16:08:55 +0100

    Arguments added

--- chromosomes.sh
+++ chromosomes.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
+### Argument is the name of the files generated from plink, if they were cattle.bed etc. then the argument would be "cattle", w/o the "".
+
+args=("$@")
 for chr in $(seq 1 29); do
-     plink --bfile clean_data \
+     plink --bfile ${args[0]} \
            --cow \
            --chr $chr \
 	   --make-bed \
-           --out cattle.chr$chr ;
+           --out ${args[0]}.chr$chr ;
 done
--- shapeit.sh
+++ shapeit.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
+### Argument is the name of the files split into individual chromosomes, if they were cattle.chr29.bed etc. then the argument would be "cattle", w/o the "".
+
+args=("$@")
 for chr in $(seq 1 29); do
-	./shapeit --input-bed cattle.chr$chr.bed cattle.chr$chr.bim cattle.chr$chr.fam \
-			  --output-max cattle.chr$chr.phased.haps cattle.chr$chr.phased.sample \
-			  --effective-size 400 \ 
+	./shapeit --input-bed ${args[0]}.chr$chr.bed ${args[0]}.chr$chr.bim ${args[0]}.chr$chr.fam \
+			  --output-max ${args[0]}$chr.phased.haps ${args[0]}$chr.phased.sample \
+			  --effective-size ${args[1]} \ 
 			  --thread 8 \
 			  --seed 123456789
 done
